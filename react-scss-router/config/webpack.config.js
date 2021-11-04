0e033b121e1a649d54d5d1e952ed4935386d38db
llqfront <llqfront@aliyun.com>
2021-11-04 10:12:16 +0800

    我们可以通过我们的webpack知识点，修改别名,import Header from ../../components/Header;

--- react-scss-router/config/webpack.config.js
+++ react-scss-router/config/webpack.config.js
@@ -334,6 +334,7 @@ module.exports = function (webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+	'@':paths.appSrc,
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
